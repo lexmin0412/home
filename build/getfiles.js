0c58e813e180a800d812e738ba78989334be5c52
cathe-zhang <zhangle_media@hotmail.com>
2020-03-29 17:33:19 +0800

    chore: 调整文章目录结构，新增config.js自动生成脚本

---
+++ build/getfiles.js
@@ -0,0 +1,103 @@
+console.log('getfiles')
+
+const fs = require('fs')
+
+/**
+ * 扫描pages文件夹生成routes.js 即app.tsx中的pages配置项
+ */
+const getPages = () => {
+  return new Promise((resolve, reject)=>{
+    console.log('开始扫描页面')
+
+    if ( fs.existsSync('./src/.vuepress/config.js') ) {
+      fs.unlinkSync('./src/.vuepress/config.js')
+      console.log('删除pages/.vuepress/config.js')
+    }
+
+    let indexLines = `module.exports = {
+      title: "CellerChan Blog",
+      description: "All I know is that I know nothing.",
+      base: "/blog/",  // basename
+      port: 8021,  // 端口
+      dest: 'docs',   // 打包输出文件存放的目录，默认为文档文件夹下的.vuepress/dist
+      markdown: {   // markdown配置对象
+        lineNumbers: true     // 是否在每个代码块的左侧显示行号
+      },
+      evergreen: false,   // 是否禁止esnext转译到es5以及对ie的polyfills，默认false 如果设置成true则只会支持现代浏览器
+      themeConfig: {
+        repo: "https://github.com/cathe-zhang/blog",
+        nav: [
+          {
+            text: 'starxing',
+            link: 'https://1415801689xing.github.io/starxingblog/'
+          }
+        ],
+        sidebar: [`
+
+    let pages = []
+
+    var outerDirs = fs.readdirSync("./src");
+
+    outerDirs.forEach((item,index)=>{
+
+      console.log('item', item)
+
+      // var innerDir = fs.readdirSync(`./src/docs/${item}`)
+
+      if ( !['images', '.vuepress', '.DS_Store', 'README.md', 'TODO.md'].includes(item) ) {
+
+        var innerDir = fs.readdirSync(`./src/${item}`)
+
+        indexLines = `${indexLines}
+          {
+            title: "${item}",
+            collapsable: false,
+            children: [`
+
+  
+        // 去除后缀名
+        innerDir.forEach((inItem,inIndex)=>{
+          if ( inItem.indexOf('.md') > -1 ) {
+            const sliceRes = inItem.slice(0,inItem.indexOf('.md'))
+            if ( inItem.indexOf('README') == -1 ) {
+              // 去重
+              if ( pages.indexOf(`${item}/${sliceRes}`) === -1 ) {
+                // pages.push(`docs/${item}/${sliceRes}`)
+                indexLines = `${indexLines}
+                  "/${item}/${sliceRes}",`
+              }
+            } else {
+              if ( pages.indexOf(`${item}/${sliceRes}`) === -1 ) {
+                // pages.push(`docs/${item}/${sliceRes}`)
+                indexLines = `${indexLines}
+                  "/${item}/",`
+              }
+            }
+          }
+        })
+
+        indexLines = `${indexLines}
+            ]
+          },`
+  
+      }
+    })
+
+    indexLines = `${indexLines}
+  ],
+    sidebarDepth: 2
+  }
+};
+    `
+    
+
+    fs.writeFileSync('./src/.vuepress/config.js', indexLines)
+    console.log('页面扫描完成，.vuepress/config.js创建成功')
+    resolve()
+  })
+
+}
+
+getPages()
+
+module.exports = getPages
--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,205 +1,243 @@
 module.exports = {
-  title: "CellerChan Blog",
-  description: "All I know is that I know nothing.",
-  base: "/blog/",  // basename
-  port: 8021,  // 端口
-  dest: 'docs',   // 打包输出文件存放的目录，默认为文档文件夹下的.vuepress/dist
-  markdown: {   // markdown配置对象
-    lineNumbers: true     // 是否在每个代码块的左侧显示行号
-  },
-  evergreen: false,   // 是否禁止esnext转译到es5以及对ie的polyfills，默认false 如果设置成true则只会支持现代浏览器
-  themeConfig: {
-    repo: "https://github.com/cathe-zhang/blog",
-    nav: [
-      {
-        text: 'starxing',
-        link: 'https://1415801689xing.github.io/starxingblog/'
-      }
-    ],
-    sidebar: [
-      "/",
-      "/前端技术栈",
-      {
-        title: "前端工程化",
-        collapsable: false,
-        children: [
-          "/前端工程化/",
-          "/前端工程化/项目文档的编写",
-          "/前端工程化/前后端分离",
-          "/前端工程化/Gitlab_协作开发流程",
-          "/前端工程化/Gitlab_Tag创建规范",
-          "/前端工程化/使用webpack搭建移动端单页应用",
-          "/前端工程化/使用rollup构建Javascript库",
-          "/前端工程化/前端项目脚手架搭建",
-          "/前端工程化/使用Github Actions自动化构建项目",
-          "/前端工程化/taro小程序mobx项目搭建",
-          "/前端工程化/taro多端项目搭建",
-          "/前端工程化/package.json配置详解"
-        ]
+      title: "CellerChan Blog",
+      description: "All I know is that I know nothing.",
+      base: "/blog/",  // basename
+      port: 8021,  // 端口
+      dest: 'docs',   // 打包输出文件存放的目录，默认为文档文件夹下的.vuepress/dist
+      markdown: {   // markdown配置对象
+        lineNumbers: true     // 是否在每个代码块的左侧显示行号
       },
-      {
-        title: "React",
-        collapsable: false,
-        children: [
-          "/React相关/React Hooks详解",
-          "/React相关/React最佳实践",
-          "/React相关/React生命周期概述",
-          "/React相关/React高阶组件",
-          "/React相关/使用react-app-rewired覆盖cra配置",
-        ]
-      },
-      {
-        title: "Vue",
-        collapsable: false,
-        children: [
-          "/Vue/vue的生命周期",
-        ]
-      },
-      {
-        title: '小程序',
-        collapsable: false,
-        children: [
-          '/小程序/微信小程序获取经纬度不准确的解决方案',
-        ]
-      },
-      {
-        title: "JavaScript",
-        collapsable: false,
-        children: [
-          "/JavaScript/"
-        ]
-      },
-      {
-        title: "杂",
-        collapsable: false,
-        children: [
-          "/HTTP请求状态码",
-          "/mac操作指南",
-          "/移动端兼容性问题解决方案",
-          "/前端技术栈",
-          "/如何写出优雅的代码",
-          "/工具箱",
-          "/常用linux命令",
-          "/常用手机软件",
-          "/使用VIM编辑和保存文件",
-          "/全栈图谱"
-        ]
-      },
-      {
-        title: 'app交互',
-        collapsable: false,
-        children: [
-          "/app交互/app webview中富文本超链接点击无反应的解决方案"
-        ]
-      },
-      {
-        title: 'CSS',
-        collapsable: false,
-        children: [
-          "/CSS/Sass的常用功能用法",
-          "/CSS/img标签底部白边处理",
-          "/CSS/使用filter属性实现高斯模糊",
-          "/CSS/样式统一",
-        ]
-      },
-      {
-        title: "ES6_plus",
-        collapsable: false,
-        children: [
-          "/ES6_plus/",
-          "/ES6_plus/ES6与commonJS模块导入导出的区别.md",
-          "/ES6_plus/Promise.md",
-          "/ES6_plus/数组方法.md"
-        ]
-      },
-      {
-        title: "Git",
-        collapsable: false,
-        children: [
-          "/Git相关/",
-          "/Git相关/git报错的解决方案.md",
-          "/Git相关/git提交日志规范.md",
-          "/Git相关/github pages配置.md",
-        ]
-      },
-      {
-        title: "H5开发",
-        collapsable: false,
-        children: [
-          "/H5开发/",
-          "/H5开发/H5定位终极解决方案.md"
-        ]
-      },
-      {
-        title: "markdown相关",
-        collapsable: false,
-        children: ["/markdown相关/"]
-      },
-      {
-        title: "npm相关",
-        collapsable: false,
-        children: [
-          "/npm相关/npm发布node包指北",
-          "/npm相关/npm常用命令.md"
-        ]
-      },
-      {
-        title: "Taro",
-        collapsable: false,
-        children: ["/Taro/Taro踩坑指南"]
-      },
-      {
-        title: "VSCode",
-        collapsable: false,
-        children: [
-          "/VSCode/vscode扩展",
-          "/VSCode/工作区相关",
-          "/VSCode/使用setting sync同步vscode设置",
-          "/VSCode/VSCode插件开发"
-        ]
-      },
-      {
-        title: '博客部署',
-        collapsable: false,
-        children: [
-          "/博客部署/vuepress部署博客",
-        ]
-      },
-      {
-        title: '测试用例',
-        collapsable: false,
-        children: [
-          '/测试用例/购物车通用测试用例',
-        ]
-      },
-      {
-        title: "编码规范",
-        collapsable: false,
-        children: [
-          "/编码规范/editorconfig配置",
-          "/编码规范/commitlint钩子配置"
+      evergreen: false,   // 是否禁止esnext转译到es5以及对ie的polyfills，默认false 如果设置成true则只会支持现代浏览器
+      themeConfig: {
+        repo: "https://github.com/cathe-zhang/blog",
+        nav: [
+          {
+            text: 'starxing',
+            link: 'https://1415801689xing.github.io/starxingblog/'
+          }
         ],
-      },
-      {
-        title: "联调的经验",
-        collapsable: false,
-        children: ["/联调的经验/与后端联调"]
-      },
-      {
-        title: '总结与展望',
-        collapsable: false,
-        children: [
-          "/总结与展望/2019年度总结",
-        ]
-      },
-      {
-        title: '点滴',
-        collapsable: false,
-        children: [
-          '/点滴/2020.01.14'
-        ]
-      }
-    ],
+        sidebar: [
+          {
+            title: "CSS",
+            collapsable: false,
+            children: [
+                  "/CSS/Sass的常用功能用法",
+                  "/CSS/img标签底部白边处理",
+                  "/CSS/使用filter属性实现高斯模糊",
+                  "/CSS/样式统一",
+            ]
+          },
+          {
+            title: "ES6_plus",
+            collapsable: false,
+            children: [
+                  "/ES6_plus/ES6与commonJS模块导入导出的区别",
+                  "/ES6_plus/Promise",
+                  "/ES6_plus/",
+                  "/ES6_plus/数组方法",
+            ]
+          },
+          {
+            title: "Git相关",
+            collapsable: false,
+            children: [
+                  "/Git相关/",
+                  "/Git相关/github pages配置",
+                  "/Git相关/git报错的解决方案",
+                  "/Git相关/git提交日志规范",
+            ]
+          },
+          {
+            title: "H5开发",
+            collapsable: false,
+            children: [
+                  "/H5开发/H5定位终极解决方案",
+                  "/H5开发/",
+            ]
+          },
+          {
+            title: "JS",
+            collapsable: false,
+            children: [
+                  "/JS/",
+            ]
+          },
+          {
+            title: "JavaScript",
+            collapsable: false,
+            children: [
+                  "/JavaScript/",
+                  "/JavaScript/构造函数",
+            ]
+          },
+          {
+            title: "React相关",
+            collapsable: false,
+            children: [
+                  "/React相关/React Context",
+                  "/React相关/React Diff算法",
+                  "/React相关/React Hooks详解",
+                  "/React相关/React最佳实践",
+                  "/React相关/React生命周期概述",
+                  "/React相关/React组件设计原则",
+                  "/React相关/React高阶组件",
+                  "/React相关/使用react-app-rewired覆盖cra配置",
+            ]
+          },
+          {
+            title: "Taro",
+            collapsable: false,
+            children: [
+                  "/Taro/Taro踩坑指南",
+            ]
+          },
+          {
+            title: "VSCode",
+            collapsable: false,
+            children: [
+                  "/VSCode/VSCode插件开发",
+                  "/VSCode/vscode扩展",
+                  "/VSCode/使用setting sync同步vscode设置",
+                  "/VSCode/工作区相关",
+            ]
+          },
+          {
+            title: "Vue",
+            collapsable: false,
+            children: [
+                  "/Vue/vue的生命周期",
+            ]
+          },
+          {
+            title: "app交互",
+            collapsable: false,
+            children: [
+                  "/app交互/app webview中富文本超链接点击无反应的解决方案",
+            ]
+          },
+          {
+            title: "markdown相关",
+            collapsable: false,
+            children: [
+                  "/markdown相关/",
+            ]
+          },
+          {
+            title: "npm相关",
+            collapsable: false,
+            children: [
+                  "/npm相关/npm发布node包指北",
+                  "/npm相关/npm常用命令",
+            ]
+          },
+          {
+            title: "其他",
+            collapsable: false,
+            children: [
+                  "/其他/HTTP请求状态码",
+                  "/其他/mac操作指南",
+                  "/其他/使用VIM编辑和保存文件",
+                  "/其他/全栈图谱",
+                  "/其他/前端客服功能的设计",
+                  "/其他/前端技术栈",
+                  "/其他/如何写出优雅的代码",
+                  "/其他/工具箱",
+                  "/其他/常用linux命令",
+                  "/其他/常用手机软件",
+                  "/其他/移动端兼容性问题解决方案",
+            ]
+          },
+          {
+            title: "前端工程化",
+            collapsable: false,
+            children: [
+                  "/前端工程化/Gitlab_Tag创建规范",
+                  "/前端工程化/Gitlab_协作开发流程",
+                  "/前端工程化/",
+                  "/前端工程化/package.json配置详解",
+                  "/前端工程化/taro多端项目搭建",
+                  "/前端工程化/taro小程序mobx项目搭建",
+                  "/前端工程化/使用Github Actions自动化构建项目",
+                  "/前端工程化/使用rollup构建Javascript库",
+                  "/前端工程化/使用webpack搭建移动端单页应用",
+                  "/前端工程化/前后端分离",
+                  "/前端工程化/前端项目脚手架搭建",
+                  "/前端工程化/框架搭建的难题",
+                  "/前端工程化/项目文档的编写",
+            ]
+          },
+          {
+            title: "前端资源集合",
+            collapsable: false,
+            children: [
+                  "/前端资源集合/",
+            ]
+          },
+          {
+            title: "博客部署",
+            collapsable: false,
+            children: [
+                  "/博客部署/vuepress部署博客",
+            ]
+          },
+          {
+            title: "回顾与展望",
+            collapsable: false,
+            children: [
+            ]
+          },
+          {
+            title: "小程序",
+            collapsable: false,
+            children: [
+                  "/小程序/微信小程序获取经纬度不准确的解决方案",
+            ]
+          },
+          {
+            title: "总结与展望",
+            collapsable: false,
+            children: [
+                  "/总结与展望/2019年度总结",
+            ]
+          },
+          {
+            title: "测试用例",
+            collapsable: false,
+            children: [
+                  "/测试用例/购物车通用测试用例",
+            ]
+          },
+          {
+            title: "点滴",
+            collapsable: false,
+            children: [
+                  "/点滴/2020.01.14",
+            ]
+          },
+          {
+            title: "用户交互",
+            collapsable: false,
+            children: [
+                  "/用户交互/",
+            ]
+          },
+          {
+            title: "编码规范",
+            collapsable: false,
+            children: [
+                  "/编码规范/commitlint钩子配置",
+                  "/编码规范/editorconfig配置",
+            ]
+          },
+          {
+            title: "联调的经验",
+            collapsable: false,
+            children: [
+                  "/联调的经验/与后端联调",
+            ]
+          },
+  ],
     sidebarDepth: 2
   }
 };
+    
